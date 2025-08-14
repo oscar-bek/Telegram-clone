eed5bea16c6c3015c0e81b83dc1098fd352c7fed
oscar-bek <salamov.behzod@gmail.com>
2025-08-14 14:51:47 +0900

    fix: update Notificaiton of getting a new message

--- models/otp.model.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require('mongoose')
-
-const otpSchema = new Schema({
-	email: { type: String, required: true },
-	otp: { type: String, required: true },
-	expireAt: { type: Date },
-})
-
-module.exports = model('Otp', otpSchema)
